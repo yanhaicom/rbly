62f4c1a6f48a2deefee46ee6c879cf6f2cc57455
yan <yanhaijie@gmail.com>
2026-03-02 23:45:15 +0900

    Reduce Prisma log level in production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,7 +9,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: process.env.NODE_ENV === 'production' ? ['error'] : ['query', 'error', 'warn'],
   });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
